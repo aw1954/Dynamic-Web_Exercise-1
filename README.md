4770a79a776dcf8311df1f35af0610eb32121c73
Aron <hw1954@nyu.edu>
2021-09-14 08:46:00 -0400

    added containers

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # Exercise One
+
+to end: ctrl+c
+
+## To push up to Github
+1. `git add .` 
+2. `git commit -m "message"`
+3. `git push origin main`
---
+++ src/containers/ArticleListing.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function ArticleListing() {
+    const articlesTitle = `Articles`
+    return (
+        <div>
+            <h1>{articlesTitle}</h1> 
+        </div> // react lets you put html in return, brackets to use js
+    )
+}
+
+export default ArticleListing
